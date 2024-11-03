138f3f4e851a0f4707175b4111f99f93d6257b5f
Yeni29 <yramoss@unah.hn>
2024-11-03 00:06:10 -0600

    actualice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from'./LOGO.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={logo} alt="Task-hive logo" className="logo" />
+      <nav>
+          <a href="#about">Nosotros</a>
+          <a href="#register">Registrarse</a>
+          <a href="#login">Iniciar sesión</a>
+          <a href="#language" className="language">Idioma</a>
+        </nav>
       </header>
+      <main className="App-main">
+        <div className="description">
+          <h1>“Organiza tu día, alcanza tus metas”</h1>
+          <p>En Task-hive, simplificamos la gestión de tus tareas diarias. Nuestra plataforma intuitiva te ayuda a organizar, priorizar y realizar un seguimiento de tus pendientes de manera efectiva. Con recordatorios inteligentes y una interfaz amigable, estarás siempre en el camino correcto para alcanzar tus objetivos.</p>
+          <p>¡Comienza a optimizar tu productividad hoy mismo!</p>
+        </div>
+        <form className="signup-form">
+          <label>
+            Nombre:
+            <input type="text" name="nombre" />
+          </label>
+          <label>
+            Apellido:
+            <input type="text" name="apellido" />
+          </label>
+          <label>
+            E-mail:
+            <input type="email" name="email" />
+          </label>
+          <label>
+            País:
+            <input type="text" name="pais" />
+          </label>
+          <button type="submit" className="submit-button">Registrarse</button>
+        </form>
+      </main>
     </div>
   );
 }
-
 export default App;
